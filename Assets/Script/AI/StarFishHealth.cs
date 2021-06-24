f88add870f8199f936c154fbbacede1fd8024c44
Angelo <angelo.estoque@yahoo.com>
2021-06-24 23:35:54 +1000

    AI Patrolling
    Changed the ai to patrol by calculating random points on the map, instead of following a set path

---
+++ Assets/Script/AI/StarFishHealth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarFishHealth : MonoBehaviour
+{
+    public float maxHealth;
+    private float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void FishTakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+            Invoke("SpawnCoral", 0.5f);
+    }
+
+    private void SpawnCoral()
+    {
+        // Disable the game object
+        gameObject.SetActive(false);
+    }
+  
+}
